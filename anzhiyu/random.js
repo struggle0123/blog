ab96d7ae252e73cac7761f5d7967b683f966bafa
struggle0123 <2391707342@qq.com>
2024-08-18 21:14:17 +0800

    Site updated: 2024-08-18 21:14:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/13/hello-world/","2024/08/17/java-script/"];function toRandomPost(){
+var posts=["2024/08/17/javascript1day/","2024/08/13/hello-world/","2024/08/18/javascript2day/","2024/08/18/javascript3day/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
